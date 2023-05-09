12ca304cc9bbbb835c5a8b500e9139d5208db29c
fakey5669 <132145609+fakey5669@users.noreply.github.com>
2023-05-09 16:47:25 +0900

    [level 0] Title: 가위 바위 보, Time: 2.17 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        char array[]=new char[rsp.length()];
+        
+        for(int i=0;i<rsp.length();i++){
+        array[i]=rsp.charAt(i);
+        }
+        
+        for(int i=0;i<rsp.length();i++){
+            if(array[i]=='2')
+                answer+='0';
+            else if(array[i]=='0')
+                answer+='5';
+            else if(array[i]=='5')
+                answer+='2';
+        }
+        return answer;
+    }
+}
+
