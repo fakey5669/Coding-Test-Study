550f31351580bff03bbbd6191c6349c3f47a8a18
fakey5669 <132145609+fakey5669@users.noreply.github.com>
2023-05-13 23:36:44 +0900

    [level 0] Title: 합성수 찾기, Time: 5.10 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,17 +1,11 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
-        int answer=0; int count=0;
-        for(int i=1;i<=n;i++){
-            answer=0;
-            for(int j=1;j<=i;j++){
-                if(i%j==0){
-                    answer++;{
-                    if(answer==3)
-                        count++; 
-                    }
-                }
-            }
-        }
-        return count;
+        return (int) IntStream.rangeClosed(1, n)
+            .filter(i -> (int) IntStream.rangeClosed(1, i) 
+                .filter(i2 -> i % i2 == 0)
+                .count() > 2) 
+            .count();
     }
 }
