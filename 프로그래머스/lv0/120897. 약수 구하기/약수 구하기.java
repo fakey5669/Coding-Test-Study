9b737dda99f5da6ca5cc69fc66691a484f3eb0c9
패키 <fakey5669@gmail.com>
2023-06-23 17:47:59 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int n) {
+        int count = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        
+        int[] divisors = new int[count]; 
+        int index = 0;
+        
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors[index] = i;
+                index++;
+            }
+        }
+        
+        return divisors;
+    }
+}
