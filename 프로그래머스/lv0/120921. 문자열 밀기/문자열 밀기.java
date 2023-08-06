52ac49865650dd632915ae655804f1ec0a585bca
패키 <fakey5669@gmail.com>
2023-08-06 21:36:44 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-
-        String tA = A;
-        for(int i = 0; i < A.length(); i++) {
-            if(tA.equals(B)) {
-                return answer;
-            }
-            String a = tA.substring(tA.length()-1);
-            tA = a + tA.substring(0, tA.length()-1);
-            answer++;
-        }
-        
-        return -1;
+        String tempB = B.repeat(2);
+        return tempB.indexOf(A);
     }
 }
