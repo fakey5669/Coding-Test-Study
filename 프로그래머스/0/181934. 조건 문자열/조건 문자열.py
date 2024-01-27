0e1c4086e255325c8ad679ac6d625a7c31050529
패키 <fakey5669@gmail.com>
2024-01-28 00:38:21 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,3 @@
+def solution(ineq, eq, n, m):
+    return int((ineq == ">" and ((eq == "=" and n >= m) or (eq == "!" and n > m))) or
+               (ineq == "<" and ((eq == "=" and n <= m) or (eq == "!" and n < m))))
