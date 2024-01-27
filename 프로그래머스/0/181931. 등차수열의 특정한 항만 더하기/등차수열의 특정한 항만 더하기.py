40776e0118e79516b1be7f042835422c3368267c
패키 <fakey5669@gmail.com>
2024-01-28 01:15:32 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,8 +1,2 @@
 def solution(a, d, included):
-    answer = 0
-    index = 0
-    for n in range(a, a + len(included) * d, d):
-        if included[index]:
-            answer += n
-        index += 1
-    return answer
+    return sum(a+i*d for i, x in enumerate(included) if x)
