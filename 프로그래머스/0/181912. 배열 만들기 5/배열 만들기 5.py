5295397b299c483779b21cb21c9ad42e2231da16
패키 <fakey5669@gmail.com>
2024-01-29 13:48:54 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.62 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,6 +1,2 @@
 def solution(intStrs, k, s, l):
-    answer = []
-    for string in intStrs:
-        if int(string[s:s+l])>k:
-            answer.append(int(string[s:s+l]))
-    return answer
+    return [int(intstr[s:s+l]) for intstr in intStrs if int(intstr[s:s+l]) > k]
