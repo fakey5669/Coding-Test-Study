45ccb9323bdb5cc77436145e0ac6085832605be1
패키 <fakey5669@gmail.com>
2024-02-11 18:58:04 +0900

    [level 0] Title: 다항식 더하기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/README.md
+++ 프로그래머스/0/120863. 다항식 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다항식 더하기 - 120863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.02 ms
+메모리: 10.4 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 16:54:53
+2024년 2월 0일 18:58:1
 
 ### 문제 설명
 
--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,5 +1,5 @@
 def solution(polynomial):
-    # 항의 계수를 저장할 변수 초기화
+    # 일차 항과 상수항의 계수를 저장할 변수
     x_coefficient = 0
     constant = 0
 
@@ -9,23 +9,28 @@ def solution(polynomial):
     for term in terms:
         # 일차 항 처리
         if 'x' in term:
-            if term == 'x':  # 계수가 생략된 경우
-                x_coefficient += 1
-            else:  # 계수가 명시된 경우
-                x_coefficient += int(term[:-1])
-        # 상수항 처리
+            if term == 'x':
+                x_coefficient += 1  # 계수 1 생략된 경우
+            else:
+                x_coefficient += int(term.replace('x', ''))  # 계수가 명시된 경우
         else:
-            constant += int(term)
+            constant += int(term)  # 상수항 처리
 
-    # 결과 문자열 생성
+    # 결과 문자열 조합
     result = []
-    if x_coefficient > 1:
-        result.append(f"{x_coefficient}x")
-    elif x_coefficient == 1:
-        result.append("x")
-    
-    if constant > 0:
+    if x_coefficient != 0:  # 일차 항이 존재하면 결과에 추가
+        if x_coefficient == 1:
+            result.append('x')
+        else:
+            result.append(f'{x_coefficient}x')
+    if constant != 0:  # 상수항이 존재하면 결과에 추가
         result.append(str(constant))
-    
-    # 결과 문자열 반환
-    return " + ".join(result) if result else "0"
+
+    # 최종 결과 문자열 반환
+    return " + ".join(result) if result else '0'
+
+# 예시
+print(solution("2x + 3 + x"))  # "3x + 3"
+print(solution("x + x + 1"))  # "2x + 1"
+print(solution("3 + 7"))  # "10"
+print(solution("x"))  # "x"
