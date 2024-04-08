9d6b572d437e186ddfe42471e1cff1582d65919f
패키 <fakey5669@gmail.com>
2024-04-08 17:12:01 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -2,9 +2,9 @@ def solution(rsp):
     answer = ''
     for i in rsp:
         if i=="2":
-            answer=answer+str(0)
+            answer+=str(0)
         elif i=="0":
-            answer=answer+str(5)
+            answer+=str(5)
         else:
-            answer=answer+str(2)
+            answer+=str(2)
     return answer
