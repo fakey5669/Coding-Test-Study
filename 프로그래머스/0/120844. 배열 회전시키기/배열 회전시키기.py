681c9d6e9c31e072fdc0d9c963c5e7fd3ed9f778
패키 <fakey5669@gmail.com>
2024-04-08 13:11:43 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,11 @@
+def solution(numbers, direction):
+    answer = [0]*len(numbers)
+    if direction=="right":
+        answer[0]=numbers[len(numbers)-1]
+        for i in range(1,len(numbers)):
+            answer[i]=numbers[i-1]
+    elif direction=="left":
+        for i in range(0,len(numbers)-1):
+            answer[i]=numbers[i+1]
+        answer[len(numbers)-1]=numbers[0]
+    return answer
