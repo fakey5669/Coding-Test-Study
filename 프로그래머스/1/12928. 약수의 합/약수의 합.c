3970af041ed229242a336968c822781b7c7b1451
패키 <fakey5669@gmail.com>
2025-04-16 11:22:49 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.c
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -4,8 +4,7 @@
 
 int solution(int n) {
     int answer = 0;
-    for(int i = 1 ; i<=3000 ; i++){
-        if(n==0) break;
+    for(int i = 1 ; i<=n ; i++){
         if(n%i==0) answer+=i;
     }
     return answer;
